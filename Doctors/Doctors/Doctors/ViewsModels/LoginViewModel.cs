1742338bcc5592f836302abcc246fb1e672faa1a
Manuel Pluas <m_pluasgo@hotmail.com>
2018-08-14 17:42:09 -0500

    BaseViewModel

--- Doctors/Doctors/Doctors/ViewsModels/LoginViewModel.cs
+++ Doctors/Doctors/Doctors/ViewsModels/LoginViewModel.cs
@@ -5,18 +5,44 @@ using System.Text;
 
 namespace Doctors.ViewsModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
-    class LoginViewModel
+    public class LoginViewModel : BaseViewModel //Esta heredando de basevM
     {
+
+        #region atributos
+        private bool isEnabled;
+        #endregion
 
         #region propiedades
         //aqui irian las propiedades que fueron mencionadas para ser
         //capturadas ejemplo
         // public String Email{get;set}
+
+        public bool IsEnabled { get; set; }
         #endregion
 
+        #region construstores
+
+
+
+
+        #endregion
         #region Comands //botones //debemos importar le s.w.Input
-        public ICommand BuscarCommand { get; set; }
+        public ICommand BuscarCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+
+            }
+        }
+
+
+        private void Login()
+        {
+
+        }
         #endregion
 
 
