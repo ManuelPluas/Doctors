bab0f34bb81e66d82668eb3e70eac38d1f98af8e
Manuel Pluas <m_pluasgo@hotmail.com>
2018-08-15 01:03:29 -0500

    LoginPage

---
+++ Doctors/Doctors/Doctors/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Doctors.Infraestructure
+{
+    using ViewsModels;
+    
+    class InstanceLocator
+    {
+        #region properties
+        public MainViewModel Main
+        { get; set; }
+
+        #endregion
+
+        #region constructores
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
